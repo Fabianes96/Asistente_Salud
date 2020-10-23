2ac86b30c60b6bdbb19ea0775e3ae39a4c807278
Daniel Vanegas <danieru1313@gmail.com>
2020-10-23 12:32:45 -0500

    Add Firebase connection

--- server.js
+++ server.js
@@ -1,5 +1,48 @@
-const express = require('express');
-const server = express();
-server.use(express.json());
-server.use(express.urlencoded({extended: false}));
+var firebase = require("firebase/app");
+require("firebase/firestore");
 
+const express = require('express');
+const app = express();
+
+
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const port = process.env.PORT;
+const firebase_config = {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+};
+
+firebase.initializeApp(firebase_config);
+const db = firebase.firestore();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+
+
+app.get('/', (req, res) => {
+    var docRef = db.collection("test").doc("xEh9EeHAcV9yl0VcLSQQ");
+    docRef.get().then(function(doc) {
+        if (doc.exists) {
+            console.log("Document data:", doc.data());
+        } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+        }
+    }).catch(function(error) {
+        console.log("Error getting document:", error);
+    });
+    res.send('Hello World!')
+  })
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
